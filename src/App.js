03db805713c9c9f808741f683b67db5528f9bb37
Fabrizio Marras <fmarras79@gmail.com>
2019-06-08 20:12:57 +0200

    [CONFIG] Folder Structure -logo is added as separate component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Logo from './components/logo';
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
